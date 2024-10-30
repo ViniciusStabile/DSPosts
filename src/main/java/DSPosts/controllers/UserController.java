a87f19b0310ecdaddb207cedc5f84c2801dec8ab
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-10-30 18:54:52 -0300

    Get user by id

--- src/main/java/DSPosts/controllers/UserController.java
+++ src/main/java/DSPosts/controllers/UserController.java
@@ -1,10 +1,35 @@
 package DSPosts.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import DSPosts.models.DTO.UserDTO;
+import DSPosts.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<UserDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		UserDTO user = service.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
 
 }
--- src/main/java/DSPosts/services/UserService.java
+++ src/main/java/DSPosts/services/UserService.java
@@ -1,6 +1,7 @@
 package DSPosts.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import DSPosts.models.DTO.UserDTO;
 import DSPosts.models.entities.User;
 import DSPosts.repositories.UserRepository;
+import DSPosts.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,6 +22,12 @@ public class UserService {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
+	}
+
+	public UserDTO findById(String id) {
+		Optional<User> user = repository.findById(id);
+		User entity = user.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
 	}
 
 }
